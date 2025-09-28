243792851d28248125dec218338303b3601709c9
cheesesuccess <natashasibanda2025@outlook.com>
2025-09-28 15:47:56 +0200

    Update route.ts

--- app/api/v1/moods/route.ts
+++ app/api/v1/moods/route.ts
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 
 const LIMIT = 10;
 
+export const dynamic: "force-dynamic" = "force-dynamic";
+
 export async function GET (request: Request) {
     try {
 
