da6f12fcb48dd99c4d05ebb4694a9568807b1452
cheesesuccess <natashasibanda2025@outlook.com>
2025-09-28 18:47:21 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,12 @@
 import { handlers } from "@/auth";
+import NextAuth from "next-auth"
+
+export const authOptions = {
+  providers: [
+    // Your providers here
+  ],
+  trustHost: true, // <-- add this
+  // ...any other config
+}
 
 export const { GET, POST } = handlers;
