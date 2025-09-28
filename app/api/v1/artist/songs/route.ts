6ad05157528f4734726afa32e7469f549b14d5db
cheesesuccess <natashasibanda2025@outlook.com>
2025-09-28 16:04:38 +0200

    Update route.ts

--- app/api/v1/artist/songs/route.ts
+++ app/api/v1/artist/songs/route.ts
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 
 const SONGS_BATCH = 10;
 
+export const dynamic: "force-dynamic" = "force-dynamic";
+
 export async function GET ( req: Request) {
     try {
         
